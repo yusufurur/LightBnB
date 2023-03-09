701ee981ee96ec6f1c9c13fa83b28e2698bd1ad7
yusufurur <yusufurur@gmail.com>
2023-03-09 17:15:36 -0500

    LighthouseBnb DDL

---
+++ LightBnB/seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
